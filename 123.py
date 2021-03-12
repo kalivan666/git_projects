ad11bec436118761b17838ff77623babe7e62811
Kalivan666 <mega.kunis@gmail.com>
2021-03-12 21:59:53 +0500

    routes

--- 123.py
+++ 123.py
@@ -1,10 +1,15 @@
 from aiohttp import web
 
 
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
 async def main(request):
     return web.Response(text="Hello, world")
 
 
 app = web.Application()
-app.add_routes([web.get('/', main)])
+app.add_routes(routes)
 web.run_app(app)
+
